d2538cc2fbe4e23a0525d59463bd34e9bbc1aec2
7att1ce <latt1ce@outlook.com>
2026-06-02 14:49:23 +0800

    update

--- 11-install-powercfg.sh
+++ 11-install-powercfg.sh
@@ -1,6 +1,8 @@
-git clone https://gitlab.com/asus-linux/nvidia-laptop-power-cfg.git && \
-cd nvidia-laptop-power-cfg && \
+git clone https://gitlab.com/asus-linux/nvidia-laptop-power-cfg.git /home/user/nvidia-laptop-power-cfg && \
+cd /home/user/nvidia-laptop-power-cfg && \
 makepkg -sfi --noconfirm && \
 sudo systemctl enable nvidia-suspend.service nvidia-hibernate.service nvidia-resume.service && \
 sudo systemctl enable --now nvidia-powerd && \
+cd /home/user && \
+rm -rf /home/user/nvidia-laptop-power-cfg && \
 echo "===== SUCCESS, RUN [reboot], NEXT: 12-install-media.sh ====="
