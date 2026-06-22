e00e2a497a5897ea342e35b9930f7e38e3997c74
Lattice <65119505+7att1ce@users.noreply.github.com>
2026-06-22 18:29:22 +0800

    Format GPU driver installation command for readability

--- 10-install-gpu-driver.sh
+++ 10-install-gpu-driver.sh
@@ -1,2 +1,5 @@
-sudo pacman -S --noconfirm --needed nvidia-open nvidia-utils lib32-nvidia-utils mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader && \
+sudo pacman -S --needed --noconfirm \
+    nvidia-open nvidia-utils lib32-nvidia-utils \
+    mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon \
+    vulkan-icd-loader lib32-vulkan-icd-loader && \
 echo "===== SUCCESS, RUN [reboot], NEXT: 11-install-powercfg.sh ====="
