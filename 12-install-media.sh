8b423fda07977b65c0ae60e88400c86fedcd5444
Lattice <65119505+7att1ce@users.noreply.github.com>
2026-06-22 18:31:06 +0800

    Refactor pacman install commands for readability

--- 12-install-media.sh
+++ 12-install-media.sh
@@ -1,4 +1,9 @@
-sudo pacman -S --noconfirm --needed libva-nvidia-driver libvdpau-va-gl libva-utils vdpauinfo vulkan-tools && \
-sudo pacman -S --noconfirm --needed sof-firmware alsa-firmware alsa-ucm-conf alsa-utils pipewire lib32-pipewire wireplumber pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack lib32-pipewire-jack && \
+sudo pacman -S --needed --noconfirm \
+    libva-nvidia-driver libvdpau-va-gl libva-utils \
+    vdpauinfo vulkan-tools && \
+sudo pacman -S --needed --noconfirm \
+    sof-firmware alsa-firmware alsa-ucm-conf alsa-utils \
+    pipewire lib32-pipewire wireplumber \
+    pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack lib32-pipewire-jack && \
 systemctl --user enable --now pipewire pipewire-pulse wireplumber && \
 echo "===== SUCCESS, RUN [reboot], NEXT: 13-install-kde-apps.sh ====="
