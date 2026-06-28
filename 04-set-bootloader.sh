86c19ec4902c88801c3ed82a42565c9ced81562f
Lattice <65119505+7att1ce@users.noreply.github.com>
2026-06-28 17:34:36 +0800

    update

--- 04-set-bootloader.sh
+++ 04-set-bootloader.sh
@@ -8,12 +8,7 @@ echo "title   Arch Linux" | tee -a /boot/loader/entries/arch.conf && \
 echo "linux   /vmlinuz-linux" | tee -a /boot/loader/entries/arch.conf && \
 echo "initrd  /amd-ucode.img" | tee -a /boot/loader/entries/arch.conf && \
 echo "initrd  /initramfs-linux.img" | tee -a /boot/loader/entries/arch.conf && \
-echo "options root=PARTUUID=${ROOT_PARTUUID} rw" | tee -a /boot/loader/entries/arch.conf && \
-echo "title   Arch Linux (dgpu only mode)" | tee -a /boot/loader/entries/arch-dgpu-only.conf && \
-echo "linux   /vmlinuz-linux" | tee -a /boot/loader/entries/arch-dgpu-only.conf && \
-echo "initrd  /amd-ucode.img" | tee -a /boot/loader/entries/arch-dgpu-only.conf && \
-echo "initrd  /initramfs-linux.img" | tee -a /boot/loader/entries/arch-dgpu-only.conf && \
-echo "options root=PARTUUID=${ROOT_PARTUUID} rw video=Unknown-1:d" | tee -a /boot/loader/entries/arch-dgpu-only.conf && \
+echo "options root=PARTUUID=${ROOT_PARTUUID} rw nvidia_drm.fbdev=1" | tee -a /boot/loader/entries/arch.conf && \
 echo "default arch.conf" | tee -a /boot/loader/loader.conf && \
 echo "timeout 3" | tee -a /boot/loader/loader.conf && \
 bootctl update && \
--- 14-fix.sh
+++ 14-fix.sh
@@ -1,4 +1,6 @@
 sudo pacman -S --noconfirm fuse2 && \
+sudo pacman -S --noconfirm unzip 7zip tmux tree firewalld ttf-cascadia-code-nerd krdc libayatana-appindicator && \
+sudo systemctl enable firewalld && \
 sudo ln -sf /usr/lib/qt6/bin/qtpaths /usr/bin/qtpaths && \
 sudo cp /etc/asusd/asusd.ron /etc/asusd/asusd.ron.bak && \
 sudo sed -i 's/platform_profile_on_ac: Performance/platform_profile_on_ac: Quiet/' /etc/asusd/asusd.ron && \
