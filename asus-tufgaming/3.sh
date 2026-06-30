2a5ee0fb47a9d2184957565725d9518b2f500529
Lattice <65119505+7att1ce@users.noreply.github.com>
2026-06-30 23:51:40 +0800

    更新 3.sh

--- asus-tufgaming/3.sh
+++ asus-tufgaming/3.sh
@@ -2,7 +2,7 @@ set -euxo pipefail
 
 git clone https://gitlab.com/asus-linux/nvidia-laptop-power-cfg.git /home/user/nvidia-laptop-power-cfg
 cd /home/user/nvidia-laptop-power-cfg
-makepkg -sfi
+makepkg -sfi --noconfirm
 sudo systemctl enable nvidia-suspend.service nvidia-hibernate.service nvidia-resume.service
 sudo systemctl enable --now nvidia-powerd
 cd /home/user
